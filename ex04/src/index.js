b28ac8b98947427ff57f28c956f30ec26b546d52
Almir2021 <bosnjicalmir@gmail.com>
2021-10-02 10:28:20 +0200

    Vjezba 4

---
+++ ex04/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+import React, {Component} from `react`;
+
+ReactDOM.render(<App/>, document.getElementById(`root`));
